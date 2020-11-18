409c572b548e2eaa29224ac262509772f32d4885
JaffarA <5521781+JaffarA@users.noreply.github.com>
2020-11-18 09:28:38 +0000

    Add files via upload

---
+++ python-2.py
@@ -0,0 +1,24 @@
+# introduction to python
+# 2 - variables
+
+# variable assignment is how we store and use information in a python program
+my_company = 'Cardif Pinnacle'
+# the code on the line above uses the '=' operator to assign a value to the 'my_company' identifier
+
+
+# an example use-case of variable assignment and manipulation.
+# in a domino notebook, we may want to assign a variable to a referenced value
+
+old_value = 8
+new_value = old_value + 8 # old_value (8) + 8 = 16
+print(new_value) # will print -> 16
+
+
+# exercise 1
+# fill in the blanks (#) below to make the final line print 'i understand variables 3'
+text_string = ''
+x = 6
+y = #
+result = #
+
+print(text_string, result)
